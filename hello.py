cf3abb7de43a40112017ebc81a3a1559dba22127
Nihar <girinihar111@gmail.com>
2024-01-20 00:25:30 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+
+num1= int(input("Enter a number :"))
+num2=int(input("Enter 2nd number :"))
+
+choice = input ("Enter the operater +, -,*,/")
+
+if choice == "+":
+    sum=num1+num2
+    print("the sum is : ", sum)
+
+if choice == '-':
+    sub=num1-num2
+    print("the sub is =" ,sub)
